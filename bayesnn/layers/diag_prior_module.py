Fixed broadcasting issue.

bayesnn/layers/diag_prior_module.py
@@ -8,6 +8,10 @@ def calculate_kl(mu_p, sig_p, mu_q, sig_q):
     return kl
 
 
+def make_positive(x, eps):
+    return F.softplus(x) + eps
+
+
 class DiagPriorModule(nn.Module):
     def __init__(self, shape, bias=True, priors={}):
         super().__init__()
@@ -33,8 +37,8 @@ class DiagPriorModule(nn.Module):
         self.posterior_mu_initial = priors['posterior_mu_initial']
         self.posterior_rho_initial = priors['posterior_rho_initial']
 
-        self.W_mu = nn.Parameter(torch.empty(shape))
-        self.W_rho = nn.Parameter(torch.empty(shape))
+        self.weight_mu = nn.Parameter(torch.empty(shape))
+        self.weight_rho = nn.Parameter(torch.empty(shape))
 
         self.use_bias = bias
 
@@ -47,11 +51,11 @@ class DiagPriorModule(nn.Module):
 
         self.reset_parameters()
 
-        self.min_std = 1e-5
+        self.min_std = 1e-8
 
     def reset_parameters(self):
-        self.W_mu.data.normal_(*self.posterior_mu_initial)
-        self.W_rho.data.normal_(*self.posterior_rho_initial)
+        self.weight_mu.data.normal_(*self.posterior_mu_initial)
+        self.weight_rho.data.normal_(*self.posterior_rho_initial)
 
         if self.use_bias:
             self.bias_mu.data.normal_(0., 1e-8)
@@ -63,27 +67,33 @@ class DiagPriorModule(nn.Module):
     def kl_bias(self, bias_mu, bias_sigma):
         return calculate_kl(self.prior_bias_mu, self.prior_bias_sigma, bias_mu, bias_sigma)
 
+    def get_weight_sigma(self):
+        return make_positive(self.weight_rho, self.min_std)
+
+    def get_bias_sigma(self):
+        return make_positive(self.bias_rho, self.min_std)
+
+    def get_kl(self):
+        kl = self.kl_weights(self.weight_mu, self.get_weight_sigma())
+        if self.use_bias:
+            kl += self.kl_bias(self.bias_mu, self.get_bias_sigma())
+        return kl
+
     def func(self, input, weight, bias):
         pass
 
     def forward(self, input, sample=True):
         if self.training or sample:
-            W_sigma = self.min_std + torch.nn.functional.softplus(self.W_rho)
-            weight = torch.distributions.Normal(self.W_mu, W_sigma).rsample()
-
-            kl = self.kl_weights(self.W_mu, W_sigma)
+            weight = torch.distributions.Normal(self.weight_mu, self.get_weight_sigma()).rsample()
 
             if self.use_bias:
-                bias_sigma = self.min_std + torch.nn.functional.softplus(self.bias_rho)
-                bias = torch.distributions.Normal(self.bias_mu, bias_sigma).rsample()
-                kl += self.kl_bias(self.bias_mu, bias_sigma)
+                bias = torch.distributions.Normal(self.bias_mu, self.get_bias_sigma()).rsample()
             else:
                 bias = None
         else:
-            weight = self.W_mu
+            weight = self.weight_mu
             bias = self.bias_mu if self.use_bias else None
-            kl = 0
 
         output = self.func(input, weight, bias)
 
-        return output, kl
+        return output

bayesnn/models/bbb_het_reg.py
@@ -10,43 +10,47 @@ sigma_w = [4, 3, 2.25, 2, 2, 1.9, 1.75, 1.75, 1.7, 1.65]
 
 
 class BBBHetRegModel(nn.Module):
-    def __init__(self, hidden_size):
+    """
+    Bayesian fully connected neural network for heteroscedastic regression following Bayes-By-Backprop algorithm.
+    Weights are stochastic, class is designed to work with stochastic variational inference.
+    """
+
+    def __init__(self, input_size=1, hidden_size=10, act_func=F.relu):
         super().__init__()
 
+        self.input_size = input_size
         self.hidden_size = hidden_size
 
         # network with two hidden and one output layer
-        self.layer1 = BBBLinear(1, self.hidden_size, priors={'prior_mu': 0,
-                                                             'prior_sigma': sigma_w[0] / np.sqrt(1),
-                                                             'posterior_mu_initial': (0, 1. / np.sqrt(
-                                                                 3. * self.hidden_size)),
-                                                             'posterior_rho_initial': (-13., 1e-8)})
-        # self.layer2 = BBBLinear(self.hidden_size, self.hidden_size, priors={'prior_mu': 0,
-        #                                                                     'prior_sigma': sigma_w[1] / np.sqrt(
-        #                                                                         self.hidden_size),
-        #                                                                     'posterior_mu_initial': (0, 1. / np.sqrt(
-        #                                                                         4. * self.hidden_size)),
-        #                                                                     'posterior_rho_initial': (-13., 1e-8)})
+        self.layer1 = BBBLinear(input_size, self.hidden_size, priors={'prior_mu': 0,
+                                                                      'prior_sigma': sigma_w[0] / np.sqrt(1),
+                                                                      'posterior_mu_initial': (0, 1. / np.sqrt(
+                                                                          3. * self.hidden_size)),
+                                                                      'posterior_rho_initial': (-13., 1e-8)})
+        self.layer2 = BBBLinear(self.hidden_size, self.hidden_size, priors={'prior_mu': 0,
+                                                                            'prior_sigma': sigma_w[1] / np.sqrt(
+                                                                                self.hidden_size),
+                                                                            'posterior_mu_initial': (0, 1. / np.sqrt(
+                                                                                4. * self.hidden_size)),
+                                                                            'posterior_rho_initial': (-13., 1e-8)})
         self.layer3 = BBBLinear(self.hidden_size, 2, priors={'prior_mu': 0,
                                                              'prior_sigma': sigma_w[2] / np.sqrt(self.hidden_size),
                                                              'posterior_mu_initial': (0, 1. / np.sqrt(
                                                                  4. * 2.)),
                                                              'posterior_rho_initial': (-13., 1e-8)})
 
-        #self.global_rho = nn.Parameter(torch.Tensor(1))
-        #self.global_rho.data.fill_(0.0001)
+        self.min_std = 1e-8
+        self.act_func = act_func
 
-        self.min_std = 1e-5
+    def get_kl(self):
+        return self.layer1.get_kl() + self.layer2.get_kl() + self.layer3.get_kl()
 
     def forward(self, input, sample=True):
-        h1, kl_1 = self.layer1(input, sample=sample)
-        #h2, kl_2 = self.layer2(nn.functional.relu(h1), sample=sample)
-        #h3, kl_3 = self.layer3(nn.functional.relu(h2), sample=sample)
-        h3, kl_3 = self.layer3(nn.functional.relu(h1), sample=sample)
+        h1 = self.layer1(input, sample=sample)
+        h2 = self.layer2(self.act_func(h1), sample=sample)
+        h3 = self.layer3(self.act_func(h2), sample=sample)
 
-        means = h3[:, 0]
-        stds = self.min_std + nn.functional.softplus(h3[:, 1]) # + nn.functional.softplus(self.global_rho)
-        #kl = kl_1 + kl_2 + kl_3
-        kl = kl_1 + kl_3
+        means = h3[:, :1]
+        stds = self.min_std + nn.functional.softplus(h3[:, 1:])
 
-        return means, stds, kl
+        return means, stds